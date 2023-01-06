3de86a66fd960faecff9f5cafcb0da9de0f6c34c
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2023-01-07 01:54:38 +0300

    training transformer example

--- 01 - Introduction to Transformers/transformer/training.py
+++ 01 - Introduction to Transformers/transformer/training.py
@@ -89,3 +89,8 @@ def run_epoch(
         del loss
         del loss_node
     return total_loss / total_tokens, train_state
+
+def loss(x, crit):
+    d = x + 3 * 1
+    predict = torch.FloatTensor([[0, x / d, 1 / d, 1 / d, 1 / d]])
+    return crit(predict.log(), torch.LongTensor([1])).data
