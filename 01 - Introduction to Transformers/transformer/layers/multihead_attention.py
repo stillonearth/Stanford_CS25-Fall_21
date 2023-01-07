5b504f2a2a86fc7af245cd256ffb89784c176380
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2023-01-07 22:01:28 +0300

    commented transformer model

--- 01 - Introduction to Transformers/transformer/layers/multihead_attention.py
+++ 01 - Introduction to Transformers/transformer/layers/multihead_attention.py
@@ -15,7 +15,7 @@ def attention(query, key, value, mask=None, dropout=None):
     p_attn = scores.softmax(dim=-1)
     if dropout is not None:
         p_attn = dropout(p_attn)
-    return torch.matmul(p_attn, value), p_attn
+    return torch.matmul(p_attn, value)
 
 
 class MultiHeadedAttention(nn.Module):
@@ -27,7 +27,6 @@ class MultiHeadedAttention(nn.Module):
         self.d_k = d_model // h
         self.h = h
         self.linears = clones(nn.Linear(d_model, d_model), 4)
-        self.attn = None
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, query, key, value, mask=None):
@@ -42,13 +41,16 @@ class MultiHeadedAttention(nn.Module):
             lin(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
             for lin, x in zip(self.linears, (query, key, value))
         ]
-
-        # 2) Apply attention on all the projected vectors in batch.
-        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
-
-        # 3) "Concat" using a view and apply a final linear.
-        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        # free some memoty befor ff pass
         del query
         del key
         del value
+
+        # 2) Apply attention on all the projected vectors in batch.
+        x = attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        # 3) "Concat" using a view and apply a final linear.
+        # align in memory
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+
         return self.linears[-1](x)
