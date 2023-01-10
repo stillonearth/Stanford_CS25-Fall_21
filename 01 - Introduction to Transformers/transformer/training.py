9d56fed9686fde3f9157e254f72504d95b365b19
Sergei Surovtsev <ssurovsev@gmail.com>
2023-01-11 01:07:01 +0300

    branch 03

--- 01 - Introduction to Transformers/transformer/training.py
+++ 01 - Introduction to Transformers/transformer/training.py
@@ -1,6 +1,14 @@
 import time
 
+import torchtext.datasets as datasets
 import torch
+
+from torch.nn.functional import pad
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from torchtext.data.functional import to_map_style_dataset
+
+from transformer.tokenizers import tokenize
 
 
 class Batch:
@@ -40,7 +48,7 @@ def run_epoch(
     scheduler,
     mode="train",
     accum_iter=1,  # Train in :acum_iter: batches before running gradient descent
-    log_ever=40,
+    log_every=40,
     train_state=TrainState(),
 ):
     """Train a single epoch"""
@@ -68,7 +76,7 @@ def run_epoch(
         total_loss += loss
         total_tokens += batch.ntokens
         tokens += batch.ntokens
-        if i % log_ever == 1 and mode == "train":
+        if i % log_every == 1 and mode == "train":
             lr = optimizer.param_groups[0]["lr"]
             elapsed = time.time() - start
             print(
@@ -91,3 +99,120 @@ def subsequent_mask(size):
     # Upper lower triangoal matrix with ones on the lower triangle and zeros on the upper triangle and diagonal
     subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1).type(torch.uint8)
     return subsequent_mask == 0
+
+
+def collate_batch(
+    batch,
+    src_pipeline,
+    tgt_pipeline,
+    src_vocab,
+    tgt_vocab,
+    device,
+    max_padding=128,
+    pad_id=2,
+):
+    bs_id = torch.tensor([0], device=device)  # <s> token id
+    eos_id = torch.tensor([1], device=device)  # </s> token id
+    src_list, tgt_list = [], []
+    for (_src, _tgt) in batch:
+        processed_src = torch.cat(
+            [
+                bs_id,
+                torch.tensor(
+                    src_vocab(src_pipeline(_src)),
+                    dtype=torch.int64,
+                    device=device,
+                ),
+                eos_id,
+            ],
+            0,
+        )
+        processed_tgt = torch.cat(
+            [
+                bs_id,
+                torch.tensor(
+                    tgt_vocab(tgt_pipeline(_tgt)),
+                    dtype=torch.int64,
+                    device=device,
+                ),
+                eos_id,
+            ],
+            0,
+        )
+        src_list.append(
+            # warning - overwrites values for negative values of padding - len
+            pad(
+                processed_src,
+                (
+                    0,
+                    max_padding - len(processed_src),
+                ),
+                value=pad_id,
+            )
+        )
+        tgt_list.append(
+            pad(
+                processed_tgt,
+                (0, max_padding - len(processed_tgt)),
+                value=pad_id,
+            )
+        )
+
+    src = torch.stack(src_list)
+    tgt = torch.stack(tgt_list)
+    return (src, tgt)
+
+
+def create_dataloaders(
+    device,
+    vocab_src,
+    vocab_tgt,
+    spacy_de,
+    spacy_en,
+    batch_size=12000,
+    max_padding=128,
+    is_distributed=True,
+):
+    # def create_dataloaders(batch_size=12000):
+    def tokenize_de(text):
+        return tokenize(text, spacy_de)
+
+    def tokenize_en(text):
+        return tokenize(text, spacy_en)
+
+    def collate_fn(batch):
+        return collate_batch(
+            batch,
+            tokenize_de,
+            tokenize_en,
+            vocab_src,
+            vocab_tgt,
+            device,
+            max_padding=max_padding,
+            pad_id=vocab_src.get_stoi()["<blank>"],
+        )
+
+    train_iter, valid_iter, test_iter = datasets.Multi30k(language_pair=("de", "en"))
+
+    train_iter_map = to_map_style_dataset(
+        train_iter
+    )  # DistributedSampler needs a dataset len()
+    train_sampler = DistributedSampler(train_iter_map) if is_distributed else None
+    valid_iter_map = to_map_style_dataset(valid_iter)
+    valid_sampler = DistributedSampler(valid_iter_map) if is_distributed else None
+
+    train_dataloader = DataLoader(
+        train_iter_map,
+        batch_size=batch_size,
+        shuffle=(train_sampler is None),
+        sampler=train_sampler,
+        collate_fn=collate_fn,
+    )
+    valid_dataloader = DataLoader(
+        valid_iter_map,
+        batch_size=batch_size,
+        shuffle=(valid_sampler is None),
+        sampler=valid_sampler,
+        collate_fn=collate_fn,
+    )
+    return train_dataloader, valid_dataloader
